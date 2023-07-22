212e22d038253b3f7e34d601e3d8d13c7e2399df
Marce Ghio <Marce.ghio.mg@gmail.com>
2023-07-22 17:56:50 -0300

    Proyecto Final React clase 15.2

--- src/services/config.js
+++ src/services/config.js
@@ -2,8 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-//"AIzaSyCv9cwmhf4jz1yVkst58tBcMRf_lq47kRU"
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "reactf-314ea.firebaseapp.com",
